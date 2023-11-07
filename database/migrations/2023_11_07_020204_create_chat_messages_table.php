70a124c5ea159bcaf2331190ebccb0baaeb20019
Kevin Caliboso <kevin.caliboso@adish.com.ph>
2023-11-07 10:29:48 +0800

    created chatroom and chat messages model w migration

---
+++ database/migrations/2023_11_07_020204_create_chat_messages_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('chat_messages', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('chatroom_id');
+            $table->index('chatroom_id');
+            $table->foreign('chatroom_id')->references('id')->on('chatrooms');
+
+            $table->unsignedBigInteger('user_id');
+            $table->index('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->unsignedBigInteger('helper_id');
+            $table->index('helper_id');
+            $table->foreign('helper_id')->references('id')->on('users');
+
+            $table->longText('message');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('chat_messages');
+    }
+};
