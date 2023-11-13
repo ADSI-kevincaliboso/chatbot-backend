fe184e8949b0604fce6d4b8d641cc4fd24ab4683
Kevin Caliboso <kevin.caliboso@adish.com.ph>
2023-11-13 15:31:46 +0800

    created user controller for admin actions

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UserRequest;
+use App\Http\Resources\UserResourceCollection;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return new UserResourceCollection(User::users()->get());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(UserRequest $request)
+    {
+        $data = $request->all();
+
+        if ($request->has('user_type')) {
+            $data['user_type'] = $request->user_type;
+        }
+        try {
+            DB::beginTransaction();
+            $user = User::create($data);
+            DB::commit();
+
+            return response()->json([
+                'message' => 'Registered successfully',
+                'data' => $user
+            ], Response::HTTP_CREATED);
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            return response()->json([
+                'message' => 'Record cannot be created'
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        try {
+            DB::beginTransaction();
+            $user->delete();
+            DB::commit();
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            return response()->json([
+                'message' => 'Record cannot be deleted'
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+}
