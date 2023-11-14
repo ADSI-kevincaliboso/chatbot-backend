df0b2d92a08fe3ba03d5aca9b17855ab616b18f2
Kevin Caliboso <kevin.caliboso@adish.com.ph>
2023-11-14 13:11:24 +0800

    removed unique on chatroom name

--- database/migrations/2023_11_07_013056_create_chatrooms_table.php
+++ database/migrations/2023_11_07_013056_create_chatrooms_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('chatrooms', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
+            $table->string('name');
             $table->unsignedBigInteger('user_id');
             $table->index('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
